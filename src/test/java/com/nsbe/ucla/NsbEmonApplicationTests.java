7da53c8cf0acddbe6a9e8600c1dfc396ce85c11c
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-11-02 17:04:53 -0400

    Init Commit

---
+++ src/test/java/com/nsbe/ucla/NsbEmonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nsbe.ucla;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NsbEmonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
